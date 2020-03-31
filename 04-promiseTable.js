2f81042af07ee90c0d8c55a2c8b7b4d67d1701ab
Wes Cutting <wesleycutting@gmail.com>
2020-03-31 10:03:09 -0600

    Handling Reject

---
+++ 04-promiseTable.js
@@ -0,0 +1,43 @@
+// Building a "Full Stack" system using TimeOuts to represent network/process delays
+// The numbers at the end of the logs are desires order
+// BACKEND - Kitchen
+const kitchen = (order) => {
+    console.log('Order received', 4); 
+    // I Owe You
+    const iou = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Food Prepared', 5);
+            resolve();
+        }, 3000);
+    });
+    return iou;
+}
+
+// FRONTEND - Table
+const table = () => {
+    console.log('Sitting at table', 2); 
+    const iou = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Ordering Food', 3);
+            // Daisy Chaining '.then()' is acceptable 
+            kitchen().then(() => {
+                console.log('Eating Food', 6);
+            }).then(()=>{
+                console.log('Paying the check', 7); 
+            }).then(() => {
+                resolve();
+            });
+        }, 1000);
+    });
+    return iou;  
+}
+
+// ENTRY POINT 
+const main = () => {
+    console.log('Entering Restaurant', 1); 
+    table().then(()=>{
+        console.log('Leaving Restaurant', 8); 
+    });
+}
+
+main();
